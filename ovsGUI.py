3dfb7aeb1e8bfdf5d628762aa868ccf6c98797df
anand.tyagi <anand.tyagi@nectechnologies.in>
2017-03-27 13:24:22 +0530

    added the style css file

--- ovsGUI.py
+++ ovsGUI.py
@@ -1,12 +1,22 @@
-from flask import Flask
+import subprocess
+from flask import Flask, render_template, request, url_for
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def first():
+    return render_template("tree.html")
+
+
+@app.route('/enter', methods=['POST'])
+def enter():
+    command = request.form.get('command_box')
+    p = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True)
+    out = p.communicate()[0]
+    context = {'out': out}
+    return render_template('home.html', **context)
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
