5736883ba14d1c495849e3b6fcf401f9ec177982
anand.tyagi <anand.tyagi@nectechnologies.in>
2017-03-27 18:58:20 +0530

    rabinanandan is typing : changed js and tree files for adding bridges

--- static/js/jsfun.js
+++ static/js/jsfun.js
@@ -15,12 +15,22 @@ for (var i = 0; i < tree.length; i++) {
         }
     });
 }
-function myFunction() {
-    document.getElementById("demo").innerHTML = httpGet("/getbridges")
-}
 function httpGet(theUrl) {
     var xmlHttp = new XMLHttpRequest();
     xmlHttp.open("GET", theUrl, false);
     xmlHttp.send(null);
     return xmlHttp.responseText;
+}
+
+function myFunction1() {
+    document.getElementById("demo1").innerHTML = httpGet("/getbridges")
+}
+function myFunction2() {
+    document.getElementById("demo2").innerHTML = httpGet("/getbridges")
+}
+function myFunction3() {
+    document.getElementById("demo3").innerHTML = httpGet("/getbridges")
+}
+function myFunction4() {
+    document.getElementById("demo4").innerHTML = httpGet("/getbridges")
 }
