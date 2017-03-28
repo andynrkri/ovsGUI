b81771044b5ff6a61bf7f98402f07f18f21f9742
anand.tyagi <anand.tyagi@nectechnologies.in>
2017-03-28 15:17:20 +0530

    returning the list of available bridges

--- ovsGUI.py
+++ ovsGUI.py
@@ -1,4 +1,4 @@
-import subprocess
+import subprocess, sqlite3
 from flask import Flask, render_template, request, url_for
 
 app = Flask(__name__)
@@ -17,20 +17,25 @@ def enter(firstparam, secondparam):
     command = [firstparam, secondparam, command, pattern, flow]
     out = get_output(command)
     context = {'out': out}
-    return render_template("output.html", command=" ".join(command))
+    return render_template("output.html", command=" ".join(command))  # need to pass in out after deploying on linux
 
 
 def get_output(command):
     p = subprocess.Popen(" ".join(command), stdout=subprocess.PIPE, shell=True)
     out = p.communicate()[0]
-    if out == None:
+    if out is None:
         out = p.communicate()[1]
     return out
 
 
+# need to return out after deployment on linux
 @app.route('/getbridges')
 def getbridges():
-    return "br0 br1 br3"
+    p = subprocess.Popen("ovs-vsctl list-br", stdout=subprocess.PIPE, shell=True)
+    out = p.communicate()[0]
+    if out is None:
+        out = p.communicate()[1]
+    return "ovs-vsctl list-br"
 
 
 if __name__ == '__main__':
